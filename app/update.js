20933e6b2939b60e7da1ae207404379001f5274d
Ian Kronquist <iankronquist@gmail.com>
2016-10-09 19:37:10 -0400

    Fix postgres number bug

--- app/update.js
+++ app/update.js
@@ -3,26 +3,28 @@
 var searchPackage = require('./search');
 
 module.exports = function(app, res, body) {
-  console.log(body);
   var knex = app.get('knex');
   knex('distros')
     .select('id')
     .where('name', body.distro)
     .first()
     .then(function(distro) {
-      console.log(distro);
-      console.log(body);
-      knex('packages').update({
+      var update = {
         name: body.package,
         install_command: body.install_command,
-        last_release: body.updated_time,
         distro: distro.id,
+        last_release: body.updated_time,
         website: body.package_website,
-      }).where({
+      };
+      if (body.updated_time == '') {
+        update.last_release = 0;
+      } else {
+        update.last_release = body.updated_time;
+      }
+      knex('packages').update(update).where({
         name: body.package,
         distro: distro.id,
       }).then(function(result) {
-        console.log(result);
         return searchPackage(app, res, body.package, distro.name);
       });
     });
