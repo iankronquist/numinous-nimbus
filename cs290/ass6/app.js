d5209943618244c487d24dc06c7ed2e6b03e12f5
Ian Kronquist <iankronquist@gmail.com>
2016-11-09 21:55:27 -0500

    ass 6 webdev

---
+++ cs290/ass6/app.js
@@ -0,0 +1,35 @@
+/*jslint node: true */
+'use strict';
+
+var express = require('express');
+var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
+
+var app_port = process.env.PORT || 8888;
+var app_ip = process.env.IP || '0.0.0.0';
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', function(req, res) {
+  console.log(req.method, req.params, req.props, req.body);
+  return res.render('index.html', {keys:Object.keys,request: req});
+});
+
+app.post('/', function (req, res) {
+  console.log(req.method, req.params, req.props, req.body);
+  return res.render('index.html', {keys:Object.keys,request: req});
+});
+
+app.listen(app_port, app_ip, function () {
+    console.log('App now listening on %s', app_port);
+});
+
+module.exports = app;
